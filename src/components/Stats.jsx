3b049bbce7dea743f9c7573c43dcd4cbb1348792
Michał Mierzejewski <mierzejewski111@gmail.com>
2024-03-26 14:51:51 +0100

    działa

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,10 +1,88 @@
-import { music } from "../assets";
+import { useState } from "react";
+import { linkedin, music } from "../assets";
 
-const Stats = () => (
-  <>
-    <div>Stats</div>
-    <iframe class="w-full aspect-video" src={music}></iframe>
-  </>
-);
+const Stats = () => {
+  const taskDate = [
+    {
+      title: "first",
+      description: "...",
+      completed: false,
+    },
+    {
+      title: "second",
+      description: "...",
+      completed: false,
+    },
+    {
+      title: "third",
+      description: "...",
+      completed: false,
+    },
+  ];
+  const [tasks, setTasks] = useState(taskDate);
+
+  const handleDelete = (index) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
+  const [newOne, setNewOne] = useState("");
+
+  const handleAdd = () => {
+    const newTasks = [...tasks];
+    newTasks.push({ title: `${newOne}`, description: "...", completed: false });
+    setTasks(newTasks);
+  };
+  const handleMade = (index) => {
+    const newTasks = [...tasks];
+    {
+      newTasks[index].completed === false
+        ? (newTasks[index].completed = true)
+        : (newTasks[index].completed = false);
+    }
+    setTasks(newTasks);
+  };
+  return (
+    <>
+      <div className="text-white">
+        <input
+          className="text-black"
+          type="text"
+          value={newOne}
+          onChange={(e) => setNewOne(e.target.value)}
+        />
+        <button onClick={newOne !== "" ? handleAdd : null}>ADD</button>
+      </div>
+
+      <div className="text-white">
+        {tasks.map((task, index) => {
+          return (
+              <li 
+                className={task.completed ? "line-through" : 'm-2' }
+                key={index}
+              >
+                {task.title}
+                <button className="ml-4 bg-white text-black rounded-lg p-1"
+                  onClick={() => {
+                    handleDelete(index);
+                  }}
+                >
+                  X
+                </button>
+                <button
+                  className="ml-4 bg-white text-black rounded-lg p-1"
+                  onClick={() => {
+                    handleMade(index);
+                  }}
+                >
+                  {task.completed ? 'U' : 'M'}
+                </button>
+              </li>
+          );
+        })}
+      </div>
+    </>
+  );
+};
 
 export default Stats;
