c908aa22ffadd688104b8cb8e1588129f91683b4
Michał Mierzejewski <mierzejewski111@gmail.com>
2024-03-28 09:04:49 +0100

    nowe

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import {
   Hero,
   Stats,
   Business,
-  Billing,
+  // Billing,
   CardDeal,
   Testimonials,
   Clients,
@@ -14,39 +14,39 @@ import {
 } from "./components";
 
 const App = () => {
-  
-const person = {
-  name: 'adam',
-  tel: 111222333,
-}
+  const person = {
+    name: "adam",
+    tel: 111222333,
+  };
 
- return (
-  <div className="bg-primary w-full overflow-hidden">
-    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Navbar />
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      <div className={` bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+      <div className={` bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business name={person.name} tel={person.tel} />
+          {/* <Billing /> */}
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
       </div>
     </div>
-
-    <div className={` bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Hero />
-      </div>
-    </div>
-
-    <div className={` bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Stats />
-        <Business name={person.name} tel={person.tel}/>
-        <Billing />
-        <CardDeal />
-        <Testimonials />
-        <Clients />
-        <CTA />
-        <Footer />
-      </div>
-    </div>
-  </div>
-)}
+  );
+};
 
 export default App;
--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
-
+import React from "react";
+import { useState } from "react";
+import "./Billing.css";
 const Billing = () => {
-  return (
-    <div>Billing</div>
-  )
-}
+  const myFields = [];
+  const botFields = [];
+  let flag1 = true;
+  const blocks = [];
+  const options = [];
 
-export default Billing
+  const startGame = () => {
+    blocks.length = 0;
+    options.length = 0;
+    myFields.length = 0;
+    botFields.length = 0;
+  };
+
+  const Reset = () => {
+    blocks.length = 0;
+    options.length = 0;
+    myFields.length = 0;
+    botFields.length = 0;
+  };
+
+  const [me, setMe] = useState()
+  return (
+    <>
+      <div className="settings">
+        <button className="reset" onClick={Reset}>Reset</button>
+        <h3 className="whoStart">Who start?</h3>
+        <form className="form1">
+          <input type="radio" className="me" value={me} onChange={() => {setMe('me')}}/>
+          me
+          <input type="radio" className="bot" /> bot
+        </form>
+        <h3 className="whoStart">Choose your item:</h3>
+        <form className="form2">
+          <input type="radio" className="x" /> X
+          <input type="radio" className="o" /> O
+        </form>
+
+        <div className="div"></div>
+      </div>
+
+      <div className="settings2">
+        <h3 className="currentPlayer"></h3>
+        <button className="accept">Accept</button>
+        <button className="decline">Cancel</button>
+      </div>
+
+      <div className="settings3">
+        <button className="Start" onClick={startGame}>
+          Start game!
+        </button>
+        <h1 className="winlosse"></h1>
+      </div>
+
+      <div className="board">
+        <div className="field" id="1"></div>
+        <div className="field" id="2"></div>
+        <div className="field" id="3"></div>
+        <div className="field" id="4"></div>
+        <div className="field" id="5"></div>
+        <div className="field" id="6"></div>
+        <div className="field" id="7"></div>
+        <div className="field" id="8"></div>
+        <div className="field" id="9"></div>
+      </div>
+    </>
+  );
+};
+
+export default Billing;
